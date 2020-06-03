dd5a99c195c1ddf7542d474a513d65125f87bb40
378333414@qq.com <378333414@qq.com>
2020-06-03 23:31:30 +0800

    第一次提交文件

---
+++ src/main/java/com/example/mydrools/mydrools/MydroolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydrools.mydrools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydroolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydroolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mydrools/mydrools/MydroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydrools.mydrools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydroolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
